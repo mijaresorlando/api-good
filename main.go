fef469cd5ed6c9c85632e977f3b1e0c6e0b76bc8
Orlando J. Acuña M <mijaresorlando@gmail.com>
2022-06-07 21:26:25 -0500

    Carga Inicial
    Carga Inicial

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+)
+
+var port = flag.Int("p", 8080, "server port")
+
+func main() {
+	flag.Parse()
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", *port), nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
